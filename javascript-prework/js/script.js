0e71fc65f92bf680fc82ab29c5dea21d54c1bfa2
w19456 <48568432+w19456@users.noreply.github.com>
2019-03-22 18:49:06 +0100

    Add "Else if"

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,4 +1,13 @@
-var computerMove, PlayerMove;
-PlayerMove = 'papier';
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + 'Jeśli Twój ruch to ' + PlayerMove + 'Wygrywasz');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
