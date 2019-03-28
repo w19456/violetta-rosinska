343b1d8916c17904347fa6ee14031a84645e363b
Viola <wiolkarosa@gmail.com>
2019-03-28 17:03:49 +0100

    titleClickHandler update

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
+    const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
@@ -11,7 +13,8 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
-
+console.log ('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
@@ -19,10 +22,13 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
